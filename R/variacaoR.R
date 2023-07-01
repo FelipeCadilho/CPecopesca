71191b3dc6401dbaf4d7bf2f562db407a2d4fb58
Felipe Cadilho <93165162+FelipeCadilho@users.noreply.github.com>
2023-07-01 05:36:37 -0300

    Update variacaoR.R

--- R/variacaoR.R
+++ R/variacaoR.R
@@ -15,27 +15,36 @@ CPecopesca <- function(penalidade="Asymptotic",
                        pen.valor=0.05,
                        metodo="PELT",
                        teste.stat="Normal",
-                       tamSegmMin=1){
-  cat("\nConfigurou o local do seu arquivo como diretório de trabalho antes de executar este pacote? S/N\n")
+                       tamSegmMin=1,
+                       un="(µm)"){
+  #aviso inicial
+  cat("\nConfigurou o local do seu arquivo de dados como
+      \ndiretório de trabalho do ambiente R antes de executar este pacote? S/N\n")
   configuracao <<- toupper(readLines(n=1))
+
   if(configuracao == "S"||configuracao == "Y"){
 
+    #pacotes necessários para o funcionamento deste pacote
     cat("\nEssa é a primeira execução após instalação deste pacote? S/N\n")
     pacote = toupper(readLines(n=1))
 
     if(pacote == "S"|| pacote == "Y"){
 
-      #chama pacote que lê planilha em excel
+      #instala e chama pacote que lê planilha em excel
       install.packages("readxl")
       library("readxl")
 
-      #chama pacote de controle de dados
+      #instala e chama pacote de controle de dados
       install.packages("dplyr")
       library("dplyr")
 
-      #chama pacote de busca por mudança em frequencia
+      #instala e chama pacote de busca por mudança em frequencia
       install.packages("changepoint")
       library("changepoint")
+
+      #instala e chama pacote de manipulação de strings
+      install.packages("stringr")
+      library("stringr")
 
     }else{
       #chama pacote que lê planilha em excel
@@ -46,8 +55,46 @@ CPecopesca <- function(penalidade="Asymptotic",
 
       #chama pacote de busca por mudança em frequencia
       library("changepoint")
+
+      #instala e chama pacote de manipulação de strings
+      library("stringr")
     }
 
+    #definição dos parâmetros da função do changepoint
+    cat("\nDeseja configurar os parametros da função changepoint? S/N\n")
+    configuracaoParametros <- toupper(readLines(n=1))
+
+    if(!is.null(configuracaoParametros)&&configuracaoParametros=="S"||configuracaoParametros=="Y"){
+      cat("\nPenalidade:\n")
+      penalidade <- readLines(n=1)
+      cat("\nValor de penalidade:\n")
+      pen.valor <- scan(n=1)
+      cat("\nMétodo:\n")
+      metodo <- readLines(n=1)
+      cat("\nTeste estatístico:\n")
+      teste.stat <- readLines(n=1)
+      cat("\nTamanho do segmento mínimo (número de observações entre as mudanças):\n")
+      tamSegmMin <- scan(n=1)
+    }
+
+    #definição da unidade de medida
+    cat("\nQual unidade de medida da distância?\n")
+    un = paste("(",readLines(n=1),")",sep="")
+
+    #definição do idioma do gráfico
+    cat("\nQual idioma do gráfico:\n1) Inglês\n2) Português\n")
+    idioma = scan(n=1)
+    if(isTRUE(idioma) && idioma==2){
+      legendaTitulo <- "Amostra:"
+      legendaX <- paste("Distância do núcleo à borda",un)
+    }else{
+      legendaTitulo <- "Sample:"
+      legendaX <- paste("Distance from core to edge",un)
+    }
+
+################################################################################
+## INÍCIO DA ENTRADA DE DADOS
+################################################################################
     #nome do arquivo de dados
     cat("\nDigite o nome do arquivo de dados com a extensão.(E.g. nome_dados.xlsx)\n")
     nome_dados <<- readLines(n=1)
@@ -101,6 +148,9 @@ CPecopesca <- function(penalidade="Asymptotic",
         dados <<- read.delim2(nome_dados, sep=separador,check.names=F, header = TRUE, encoding = "UTF-8")
       }
     }
+################################################################################
+## FIM DA ENTRADA DE DADOS
+################################################################################
 
     #identificação da amostra
     cat("\nInforme a identificação da amostra.\n")
@@ -119,17 +169,25 @@ CPecopesca <- function(penalidade="Asymptotic",
     elementos <<- length(dados)
 
     #obtém a identificação da amostra
-    amostra <<- paste("Sample:",amostra)
+    amostra <<- paste(legendaTitulo,amostra)
 
     #cria dataframe que alocará os resultados
     resultados <<- data.frame()
+    localiza <<- data.frame()
+    media <<- data.frame()
     resultados[1,1] <<- "Posição do ponto de mudança"
     resultados[2,1] <<- "Valor do elemento no ponto de mudança"
     resultados[3,1] <<- "Médias entre as mudanças"
     resultados[4,1] <<- "Valor da distância no ponto de mudança"
     colnames(resultados) <<- c("DESCRIÇÃO")
 
+    #nomes para comparação
+    dfnomes <<- data.frame()
+    dfnomes[1,1] <<- "dist"
 
+################################################################################
+## INÍCIO DA REPETIÇÃO
+################################################################################
     #repetição para gerar os gráficos por elemento
     for(a in 2:elementos){
 
@@ -146,22 +204,28 @@ CPecopesca <- function(penalidade="Asymptotic",
                           param.estimates=TRUE,
                           minseglen=tamSegmMin)
 
-      #quantidade total de linhas da coluna distância
-      limiteDistanciaMax <<- length(dados[,1])
-
-      #menor contagem de linhas possível da coluna distância
+      #linha inicial da coluna distância
       limiteDistanciaMin <<- 1
 
       #quantidade de pontos de mudanças
       limitePontos <<- length(mudanca@cpts)
 
+      #nomes para comparação
+      tt <<- a-1
+      dfnomes[1,tt] <<- paste(tt,": ",nome,sep="")
+      colnames(dfnomes)[tt] <<- c(dfnomes[1,tt])
+
       #alocação dos resultados
       for(o in 1:limitePontos){
         resultados[1,a] <<- paste(resultados[1,a],mudanca@cpts[o])
+        localiza[o,tt] <<- mudanca@cpts[o]
         resultados[2,a] <<- paste(resultados[1,a],dados[mudanca@cpts[o],a])
         resultados[3,a] <<- paste(resultados[1,a],mudanca@param.est$mean[o])
+        media[o,tt] <<- mudanca@param.est$mean[o]
         resultados[4,a] <<- paste(resultados[1,a],dados[mudanca@cpts[o],1])
       }
+
+      #nome do elemento atual
       colnames(resultados)[a] <<- c(nome)
 
       if(mudaEixoY==1){
@@ -175,20 +239,20 @@ CPecopesca <- function(penalidade="Asymptotic",
 
           #geração do gráfico que receberá posteriormente as linhas de mudança
           plot(x = dados[,1], y = dados[,a], type = "l",
-               xlab = "Distance (µm)",
+               xlab = paste(legendaX),
                ylab = nome,
                ylim = c(yMin,yMax))
           title(main = amostra,
-                xlab = "Distance (µm)",
+                xlab = paste(legendaX),
                 ylab = nome)
       }else{
-          #geração do gráfico que receberá posteriormente as linhas de mudança
-          plot(x = dados[,1], y = dados[,a], type = "l",
-               xlab = "Distance (µm)",
-               ylab = nome)
-          title(main = amostra,
-                xlab = "Distance (µm)",
-                ylab = nome)
+        #geração do gráfico que receberá posteriormente as linhas de mudança
+        plot(x = dados[,1], y = dados[,a], type = "l",
+             xlab = paste(legendaX),
+             ylab = nome)
+        title(main = amostra,
+              xlab = paste(legendaX),
+              ylab = nome)
       }
       #repetição pela quantidade de pontos de mudanças
       for(i in 1:limitePontos){
@@ -222,14 +286,54 @@ CPecopesca <- function(penalidade="Asymptotic",
           limiteDistanciaMin <<- mudanca@cpts[i]+1
       }
     }
+################################################################################
+## FIM DA REPETIÇÃO
+################################################################################
+
     #removedor de objetos
     rm(linhas, envir = .GlobalEnv)
     rm(limiteDistanciaMin, envir = .GlobalEnv)
-    rm(limiteDistanciaMax, envir = .GlobalEnv)
     rm(limitePontos, envir = .GlobalEnv)
     rm(elementos, envir = .GlobalEnv)
     rm(nome, envir = .GlobalEnv)
     rm(configuracao, envir = .GlobalEnv)
+    rm(configuracaoParametros, envir = .GlobalEnv)
+
+    #comparação entre elementos
+    cat("\nDeseja comparar elementos:\n0) Não comparar\n1) 2 elementos\n2) 3 elementos\n")
+    compara <- scan(n=1)
+
+    elementosSelecionados <- c()
+    localizacoes <<- localiza
+    medias <<- media
+    if(!is.null(compara) && compara!=0){
+      cat("\nElementos a serem comparados:\n",colnames(dfnomes),"\n")
+
+      if(compara == 1){#dois elementos
+        for(i in 1:2){
+          cat("\nDigite o elemento",i,":\n")
+          elementosSelecionados[i] <- scan(n=1)
+          localizacoes[,i] <<- localiza[,elementosSelecionados[i]]
+          medias[,i] <<- media[,elementosSelecionados[i]]
+          elementosSelecionados[i] <- elementosSelecionados[i]+1
+        }
+      }else if(compara == 2){#três elementos
+        for(i in 1:3){
+          cat("\nDigite o elemento",i,":\n")
+          elementosSelecionados[i] <- scan(n=1)
+          localizacoes[,i] <<- localiza[,elementosSelecionados[i]]
+          medias[,i] <<- media[,elementosSelecionados[i]]
+          elementosSelecionados[i] <- elementosSelecionados[i]+1
+        }
+      }
+      comparacao(compara, elementosSelecionados, localizacoes, medias, dados, amostra, legendaX)
+    }
+
+    #removedor de objetos
+    rm(dfnomes, envir = .GlobalEnv)
+    rm(media, envir = .GlobalEnv)
+    rm(tt, envir = .GlobalEnv)
+
     write.table(resultados, file='resultados-PCecopesca.csv', sep=',', dec='.', row.names=FALSE)
     return("Processo finalizado.")
   }else{
